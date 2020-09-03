ea40514fc1d47ab2e7decc0b5dd791b27a17b9c3
adonsav <adonsav@gmail.com>
2020-09-03 19:23:05 +0300

    Create 'SplashActivity.kt'

---
+++ app/src/main/java/com/example/culpix/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.example.culpix
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.culpix.databinding.ActivitySplashBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+    private lateinit var auth: FirebaseAuth
+    private val handler: Handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this@SplashActivity, R.layout.activity_splash)
+
+        auth = FirebaseAuth.getInstance()
+        handler.postDelayed({
+            val intent: Intent
+            if (auth.currentUser == null) {
+                // TODO Create ConnectActivity
+                //intent = Intent(this@SplashActivity, ConnectActivity::class.java)
+                // TODO Start ConnectActivity
+                //startActivity(intent)
+                finish()
+            } else {
+                // TODO Call the proper activity when ready
+            }
+        }, 2000)
+
+    }
+
+}
