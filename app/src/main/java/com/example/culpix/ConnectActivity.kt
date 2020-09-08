6741e214960897aacc686b48541d03e64089090c
adonsav <adonsav@gmail.com>
2020-09-08 09:21:39 +0300

    Set click listener for 'loginBtn'

--- app/src/main/java/com/example/culpix/ConnectActivity.kt
+++ app/src/main/java/com/example/culpix/ConnectActivity.kt
@@ -1,5 +1,6 @@
 package com.example.culpix
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -12,6 +13,11 @@ class ConnectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@ConnectActivity, R.layout.activity_connect)
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this@ConnectActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
-    
+
 }
