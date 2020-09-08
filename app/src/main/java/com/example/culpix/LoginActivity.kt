99b54c036311cdb7a370ee176dbfa4aebcd9ebe5
adonsav <adonsav@gmail.com>
2020-09-08 09:20:57 +0300

    Use proper activity name

--- app/src/main/java/com/example/culpix/LoginActivity.kt
+++ app/src/main/java/com/example/culpix/LoginActivity.kt
@@ -3,15 +3,15 @@ package com.example.culpix
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.example.culpix.databinding.ActivitySplashBinding
+import com.example.culpix.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@LoginActivity, R.layout.activity_login)
     }
-    
+
 }
